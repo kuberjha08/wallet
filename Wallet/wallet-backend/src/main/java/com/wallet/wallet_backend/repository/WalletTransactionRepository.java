0aecd6f4d95765517c8938c677410c6cf383272d
kuberjha08 <kuberjha08@gmail.com>
2026-02-13 11:57:24 +0530

    initiall

--- Wallet/wallet-backend/src/main/java/com/wallet/wallet_backend/repository/WalletTransactionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.wallet.wallet_backend.repository;
-
-import com.wallet.wallet_backend.entity.WalletTransaction;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface WalletTransactionRepository extends JpaRepository<WalletTransaction, Long> {
-    List<WalletTransaction> findByUserIdOrderByCreatedAtDesc(Long userId);
-}
